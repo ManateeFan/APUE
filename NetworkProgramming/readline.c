db4dcf1b76cbb26155c165170ccfd929fc913d91
ManateeFan <0219lyf@gmail.com>
2020-04-22 22:55:26 +0800

    Update

---
+++ NetworkProgramming/readline.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+ssize_t
+readline(int fd, void *vptr, size_t maxlen)
+{
+    ssize_t n, rc;
+    char c, *ptr;
+
+    ptr = vptr;
+    for (n = 1; n < maxlen; n++)
+    {
+        if ((rc = my_read(fd, &c)) == 1)
+        {
+            *ptr++ = c;
+            if (c == '\n')
+                break; /* newline is stored, like fgets() */
+        }
+        else if (rc == 0)
+        {
+            *ptr = 0;
+            return (n - 1); /* EOF, n - 1 bytes were read */
+        }
+        else
+            return (-1); /* error, errno set by read() */
+    }
+
+    *ptr = 0; /* null terminate like fgets() */
+    return (n);
+}
+
+ssize_t
+Readline(int fd, void *ptr, size_t maxlen)
+{
+    ssize_t n;
+
+    if ((n = readline(fd, ptr, maxlen)) < 0)
+    {
+        perror("readline error");
+        exit(1);
+    }
+    return (n);
+}
