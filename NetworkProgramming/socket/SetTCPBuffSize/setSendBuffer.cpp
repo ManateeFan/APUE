45b85867878fc31e65de8a536618f30e5c8be337
ManateeFan <0219lyf@gmail.com>
2020-04-25 12:49:59 +0800

    set tcp send buffer size

---
+++ NetworkProgramming/socket/SetTCPBuffSize/setSendBuffer.cpp
@@ -0,0 +1,45 @@
+#include <arpa/inet.h>
+#include <assert.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc <= 2)
+    {
+        printf("usage:%s ip_address port_number send_bufer_size\n", argv[0]);
+        return 1;
+    }
+    const char *ip = argv[1];
+    int port = atoi(argv[2]);
+
+    struct sockaddr_in serverAddr;
+    bzero(&serverAddr, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &serverAddr.sin_addr);
+    serverAddr.sin_port = htons(port);
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    assert(sock >= 0);
+
+    int sendbuf = atoi(argv[3]);
+    int len = sizeof(sendbuf);
+    /* 设置tcp发送缓冲区大小*/
+    setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &sendbuf, sizeof(sendbuf));
+    getsockopt(sock, SOL_SOCKET, SO_SNDBUF, &sendbuf, (socklen_t *)&len);
+    printf("the tcp send buffer size after setting is %d\n", sendbuf);
+
+    if ((connect(sock, &serverAddr, sizeof(serverAddr))) != -1)
+    {
+        char buffer[BUFSIZ];
+        memset(buffer, 'a', BUFSIZ);
+        send(sock, buffer, BUFSIZ, 0);
+    }
+
+    close(sock);
+    return 0;
+}
