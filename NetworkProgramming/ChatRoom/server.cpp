3e624ca80ab9735ccddf6d388e2e3804574db759
Yifan Liu <43963430+ManateeFan@users.noreply.github.com>
2020-04-28 13:14:10 +0800

    Update server.cpp

--- NetworkProgramming/ChatRoom/server.cpp
+++ NetworkProgramming/ChatRoom/server.cpp
@@ -9,8 +9,27 @@
 #include <stdlib.h>
 #include <poll.h>
 #include <fcntl.h>
+#include <errno.h>
 
-constexpr int BUFFER_SIZE = 64;
+constexpr int USER_LIMIT = 5;   /* 最大用户数量 */
+constexpr int BUFFER_SIZE = 64; /* 读缓冲区大小 */
+constexpr int FD_LIMIT = 65535; /* 文件描述符数量限制 */
+/* 客户数据：客户socket地址、待写到客户端的数据的位置、从客户端读入的数据 */
+struct client_data
+{
+    sockaddr_in address;
+    char *write_buf = nullptr;
+    char buf[BUFFER_SIZE];
+};
+
+/* 设置文件描述符非阻塞 */
+int setNonBlocking(int fd)
+{
+    int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_option);
+    return old_option;
+}
 
 int main(int argc, char *argv[])
 {
@@ -22,62 +41,140 @@ int main(int argc, char *argv[])
     const char *ip = argv[1];
     int port = atoi(argv[2]);
 
-    struct sockaddr_in server_address;
-    bzero(&server_address, sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    inet_pton(AF_INET, ip, &server_address.sin_addr);
-    server_address.sin_port = htons(port);
+    struct sockaddr_in address;
+    bzero(&address, sizeof(address));
+    address.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &address.sin_addr);
+    address.sin_port = htons(port);
 
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    assert(sockfd >= 0);
-    if (connect(sockfd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
-    {
-        printf("connection failed\n");
-        close(sockfd);
-        return 1;
-    }
+    int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    assert(listenfd >= 0);
 
-    pollfd fds[2];
-    /* 注册文件描述符0（标准输入）的读事件 */
-    fds[0].fd = 0;
-    fds[0].events = POLLIN;
-    fds[0].revents = 0;
-    /* 注册文件描述符sockfd的读事件 */
-    fds[1].fd = sockfd;
-    fds[1].events = POLLIN | POLLRDHUP;
-    fds[1].revents = 0;
-
-    char read_buf[BUFFER_SIZE];
-    int pipefd[2];
-    int ret = pipe(pipefd);
+    int ret = bind(listenfd, (struct sockaddr *)&address, sizeof(address));
     assert(ret != -1);
+
+    ret = listen(listenfd, 5);
+    assert(ret != -1);
+
+    /* users数组，分配FD_LIMIT个client_data对象。每个socket都能获得这样一个对象，socket的值作为索引 */
+    client_data *users = new client_data[FD_LIMIT];
+
+    pollfd fds[USER_LIMIT + 1];
+    int user_counter = 0;
+    for (int i = 1; i <= user_counter; ++i)
+    {
+        fds[i].fd = -1;
+        fds[i].events = 0;
+    }
+    fds[0].fd = listenfd;
+    fds[0].events = POLLIN | POLLERR;
+    fds[0].revents = 0;
 
     while (true)
     {
-        ret = poll(fds, 2, -1);
+        ret = poll(fds, user_counter + 1, -1);
         if (ret < 0)
         {
             printf("poll failure\n");
             break;
         }
-        if (fds[1].revents & POLLRDHUP)
+        /* 遍历fds数组，查询事件 */
+        for (int i = 0; i < user_counter + 1; ++i)
         {
-            printf("server close the connection\n");
-            break;
-        }
-        else if (fds[1].revents & POLLIN)
-        {
-            memset(read_buf, '\0', BUFFER_SIZE);
-            recv(fds[1].fd, read_buf, BUFFER_SIZE - 1, 0);
-            printf("%s\n", read_buf);
-        }
-        if (fds[0].revents & POLLIN)
-        {
-            /* splice实现将用户输入之间写到sockfd的内核发送缓冲区 */
-            splice(STDIN_FILENO, nullptr, pipefd[1], nullptr, 32768, SPLICE_F_MORE | SPLICE_F_MOVE);
-            splice(pipefd[0], nullptr, sockfd, nullptr, 32768, SPLICE_F_MORE | SPLICE_F_MOVE);
+            if (fds[i].fd == listenfd && fds[i].revents & POLLIN)
+            {
+                struct sockaddr_in client_address;
+                socklen_t client_addrlength = sizeof(client_address);
+                int connfd = accept(listenfd, (struct sockaddr *)&client_address, &client_addrlength);
+                if (connfd < 0)
+                {
+                    perror("accept error");
+                    continue;
+                }
+                /* 用户数量超出限制，关闭新到的连接 */
+                if (user_counter >= USER_LIMIT)
+                {
+                    const char *info = "too many users\n";
+                    close(connfd);
+                    continue;
+                }
+                /* 新的连接，同时修改fds数组和users数组 */
+                user_counter++;
+                users[connfd].address = client_address;
+                setNonBlocking(connfd);
+                fds[user_counter].fd = connfd;
+                fds[user_counter].events = POLLIN | POLLRDHUP | POLLERR;
+                fds[user_counter].revents = 0;
+                printf("comes a new user, now have %d users\n", user_counter);
+            }
+            else if (fds[i].revents & POLLERR)
+            {
+                printf("get an error form %d\n", fds[i].fd);
+                char errors[100];
+                memset(errors, '\0', 100);
+                socklen_t length = sizeof(errors);
+                if (getsockopt(fds[i].fd, SOL_SOCKET, SO_ERROR, &errors, &length) < 0)
+                {
+                    printf("get socket option failed\n");
+                }
+                continue;
+            }
+            else if (fds[i].revents & POLLRDHUP)
+            {
+                /* 客户端关闭了连接，则服务器也关闭对应连接，用户数-1 */
+                users[fds[i].fd] = users[fds[user_counter].fd];
+                close(fds[i].fd);
+                fds[i] = fds[user_counter];
+                i--;
+                user_counter--;
+                printf("a client left\n");
+            }
+            else if (fds[i].revents & POLLIN)
+            {
+                int connfd = fds[i].fd;
+                memset(users[connfd].buf, '\0', BUFFER_SIZE);
+                ret = recv(connfd, users[connfd].buf, BUFFER_SIZE - 1, 0);
+                printf("get %d bytes of client data %s form %d \n", ret, users[connfd].buf, connfd);
+                if (ret < 0)
+                {
+                    if (errno != EAGAIN)
+                    {
+                        close(connfd);
+                        users[fds[i].fd] = users[fds[user_counter].fd];
+                        fds[i] = fds[user_counter];
+                        i--;
+                        user_counter--;
+                    }
+                }
+                else if (ret == 0)
+                {
+                }
+                else
+                {
+                    /* 接收到客户数据，并通知其他socket准备写数据 */
+                    for (int j = 1; j <= user_counter; ++j)
+                    {
+                        if (fds[j].fd == connfd)
+                            continue;
+                        fds[j].events |= ~POLLIN;
+                        fds[j].events |= POLLOUT;
+                        users[fds[j].fd].write_buf = users[connfd].buf;
+                    }
+                }
+            }
+            else if (fds[i].revents & POLLOUT)
+            {
+                int connfd = fds[i].fd;
+                if (!users[connfd].write_buf)
+                    continue;
+                ret = send(connfd, users[connfd].write_buf, strlen(users[connfd].write_buf), 0);
+                users[connfd].write_buf = nullptr;
+                fds[i].events |= ~POLLOUT;
+                fds[i].events |= POLLIN;
+            }
         }
     }
-    close(sockfd);
+    delete[] users;
+    close(listenfd);
     return 0;
 }
