7b3f732330a26f2c3c2802c0de363b4ae5b4d5fa
Yifan Liu <43963430+ManateeFan@users.noreply.github.com>
2020-07-28 09:23:07 +0800

    Update client.c

--- client.c
+++ client.c
@@ -24,17 +24,19 @@ int main(int argc, char *argv[])
         printf("usage: %s ip_address port_number\n", basename(argv[0]));
         return 1;
     }
+    /* 根据命令行参数，获取ip和端口号 */
     const char *ip = argv[1];
     int port = atoi(argv[2]);
 
     const char* name = "localhost";
+    /* 通过本地地址获取主机信息 */
     struct hostent* host = gethostbyaddr(name, 4, AF_INET);
     char buf[BUFFER_SIZE];
     bzero(buf, BUFFER_SIZE);
     strcpy(buf, host->h_name);
     strcat(buf, ":");
 
-
+    /* 设置服务器socket地址结构体 */
     struct sockaddr_in server_address;
     bzero(&server_address, sizeof(server_address));
     server_address.sin_family = AF_INET;
@@ -43,6 +45,7 @@ int main(int argc, char *argv[])
 
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     assert(sockfd >= 0);
+    /* 连接该服务器 */
     if (connect(sockfd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
     {
         printf("connection failed\n");
@@ -61,14 +64,10 @@ int main(int argc, char *argv[])
     fds[1].revents = 0;
 
     char read_buf[BUFFER_SIZE];
-    int pipefd[2];
-    int ret = pipe(pipefd);
-    assert(ret != -1);
-
 
     while (1)
     {
-        ret = poll(fds, 2, -1);
+        int ret = poll(fds, 2, -1);
         if (ret < 0)
         {
             printf("poll failure\n");
